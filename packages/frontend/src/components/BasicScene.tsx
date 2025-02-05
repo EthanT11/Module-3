d2e7871a32b6c2ec9671baaf704d643cb14e0295
Ethan <61360788+EthanT11@users.noreply.github.com>
2025-02-05 15:08:45 -0330

    feat(BasicScene.tsx): Added Player. Created function to create player. Attached the Universal camera to the player. Should move with the player once we handle movement and updates

--- packages/frontend/src/components/BasicScene.tsx
+++ packages/frontend/src/components/BasicScene.tsx
@@ -1,5 +1,26 @@
-import { Engine, Scene, Vector3, HemisphericLight, UniversalCamera, MeshBuilder } from "@babylonjs/core"
+import { Engine, Scene, Vector3, HemisphericLight, UniversalCamera, MeshBuilder, Mesh } from "@babylonjs/core"
 import { useRef, useEffect } from "react"
+
+// TODO: Add a player character
+// TODO: handle movement | Need a control scheme that's easy to use for the player WASD and mouse only
+// TODO: Add a camera to the player
+// TODO: Start factoring out the scene props when this gets too big
+
+const PLAYER_CONFIG = {
+    size: 1,
+    position: {
+        x: 4,
+        y: 1,
+    }
+}
+
+const createPlayer = (scene: Scene): Mesh => { // Should return a player mesh
+    const player = MeshBuilder.CreateBox("player", { size: PLAYER_CONFIG.size}, scene);
+    player.position.y = PLAYER_CONFIG.position.y;
+    player.position.x = PLAYER_CONFIG.position.x;
+    return player
+}
+
 
 const CreateBasicScene = () => {
     const reactCanvas = useRef(null); // Use useRef to store the canvas element
@@ -14,9 +35,15 @@ const CreateBasicScene = () => {
         const engine = new Engine(canvas, true) // First argument is the canvas element, second argument is antialiasing
         const scene = new Scene(engine) // Create a new scene
 
-        const camera = new UniversalCamera("camera", new Vector3(4, 15, -20), scene) // Create a new camera | UniversalCamera is a camera that can be controlled by the mouse and arrow keys and gamepad which could be interesting
-        camera.setTarget(Vector3.Zero()) // Set the target of the camera to x/y/z 0/0/0
-        camera.attachControl(canvas, true)
+        const player = createPlayer(scene)
+        const playerInput = new UniversalCamera("playerInput", new Vector3(0, 0, 0), scene)
+        playerInput.attachControl(canvas, true)
+        playerInput.setTarget(player.position)
+
+
+        // const camera = new UniversalCamera("camera", new Vector3(4, 15, -20), scene) // Create a new camera | UniversalCamera is a camera that can be controlled by the mouse and arrow keys and gamepad which could be interesting
+        // camera.setTarget(Vector3.Zero()) // Set the target of the camera to x/y/z 0/0/0
+        // camera.attachControl(canvas, true)
 
         const light = new HemisphericLight("light", new Vector3(0, 20, 0), scene) // Create a new light
         light.intensity = 0.7 // From 0 to 1
