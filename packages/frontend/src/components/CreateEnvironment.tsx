913f466b3962e4847666cbc01973a2f16f1402dd
Ethan <61360788+EthanT11@users.noreply.github.com>
2025-02-06 15:14:30 -0330

    chore: Cleaned up CreateEnvironment file. Added better error handling to each function. Added better comments to the code especially with the materials since that's 100% new to me

--- packages/frontend/src/components/CreateEnvironment.tsx
+++ packages/frontend/src/components/CreateEnvironment.tsx
@@ -129,20 +129,21 @@ const createGroundMaterial = (scene: Scene): StandardMaterial => {
         const textueArray: Texture[] = [];
         
         const diffuseTexture = new Texture("/textures/rocky_terrain/rocky_terrain_diffuse.jpg", scene);
-        material.diffuseTexture = diffuseTexture;
+        material.diffuseTexture = diffuseTexture; // A diffuse texture is a texture that is used to change the color of the material
         textueArray.push(diffuseTexture);
 
         const normalTexture = new Texture("/textures/rocky_terrain/rocky_terrain_normal.jpg", scene);
-        material.bumpTexture = normalTexture; 
+        material.bumpTexture = normalTexture; // A bump texture is a texture that is used to change the normal of the material | Normal is the direction of the material
         textueArray.push(normalTexture);
 
         const aoTexture = new Texture("/textures/rocky_terrain/rocky_terrain_ao.jpg", scene);
-        material.ambientTexture = aoTexture;
+        material.ambientTexture = aoTexture; // A ambient texture is a texture that is used to change the ambient of the material | Ambient is the overall color of the material
         textueArray.push(aoTexture);
 
         const specularTexture = new Texture("/textures/rocky_terrain/rocky_terrain_spec.jpg", scene);
-        material.specularTexture = specularTexture;
+        material.specularTexture = specularTexture; // A specular texture is a texture that is used to change the specular of the material | Specular is the highlight of the material
         textueArray.push(specularTexture);
+
 
         textueArray.forEach(texture => { // Apply scale to each texture
             texture.uScale = uvScale;
