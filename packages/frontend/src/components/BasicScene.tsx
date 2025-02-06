54fa7d334ab37bf90f844c2fa6b229903194f415
Ethan <61360788+EthanT11@users.noreply.github.com>
2025-02-06 11:26:25 -0330

    feat(BasicScene): Added gravity to the camera and collision to the ground and camera.

--- packages/frontend/src/components/BasicScene.tsx
+++ packages/frontend/src/components/BasicScene.tsx
@@ -1,50 +1,66 @@
-import { Engine, Scene, Vector3, HemisphericLight, UniversalCamera, MeshBuilder, Mesh } from "@babylonjs/core"
+import { Engine, Scene, Vector3, HemisphericLight, UniversalCamera, MeshBuilder, Mesh, Color4 } from "@babylonjs/core"
 import { useRef, useEffect } from "react"
 
 // TODO: handle movement | Need a control scheme that's easy to use for the player WASD and mouse only
 // TODO: Add a camera to the player
 // TODO: Start factoring out the scene props when this gets too big
 
+// RESOURCES
+// https://doc.babylonjs.com/features/featuresDeepDive/cameras/camera_collisions
+// https://www.youtube.com/watch?v=npt_oXGTLfg
+
 // configs
-const PLAYER_CONFIG = {
-    position: {
-        x: 4,
-        y: 1,
+const SCENE_CONFIG = {
+    PLAYER_CONFIG: {
+        position: new Vector3(4, 1, 0),
+        size: 1,
+        speed: 0.1,
     },
-    size: 1,
-    speed: 0.1,
+    GROUND_CONFIG: {
+        width: 125,
+        height: 121,
+    },
+    LIGHT_CONFIG: {
+        intensity: 0.8, // 0 to 1
+        position: new Vector3(0, 20, 0),
+    },
+
 }
 
-const GROUND_CONFIG = {
-    width: 125,
-    height: 121,
-}
+const GRAVITY = -9.81;
+const FPS = 60;
 
-const LIGHT_CONFIG = {
-    intensity: 0.8,
-    position: new Vector3(0, 20, 0),
-}
+// const createPlayer = (scene: Scene): Mesh => { // Should return a player mesh
+//     const player = MeshBuilder.CreateBox("player", { size: SCENE_CONFIG.PLAYER_CONFIG.size}, scene);
+//     player.position.y = SCENE_CONFIG.PLAYER_CONFIG.position.y;
+//     player.position.x = SCENE_CONFIG.PLAYER_CONFIG.position.x;
 
-const createPlayer = (scene: Scene): Mesh => { // Should return a player mesh
-    const player = MeshBuilder.CreateBox("player", { size: PLAYER_CONFIG.size}, scene);
+//     player.isVisible = true; // Hide the player mesh
 
+//     return player
+// }
 
-    player.position.y = PLAYER_CONFIG.position.y;
-    player.position.x = PLAYER_CONFIG.position.x;
-    return player
-}
+const setupCamera = (scene: Scene, canvas: HTMLCanvasElement): UniversalCamera => {
+    const camera = new UniversalCamera(
+        "camera", 
+        new Vector3(1, 1, 1), // Start position of the camera
+        scene
+    );
 
-const setupCamera = (scene: Scene, canvas: HTMLCanvasElement, target: Mesh): UniversalCamera => {
-    const camera = new UniversalCamera("camera", new Vector3(0, 0, -10), scene);
     camera.attachControl(canvas, true);
-    camera.setTarget(target.position);
+    camera.applyGravity = true;
+    camera.checkCollisions = true;
+    camera.ellipsoid = new Vector3(1, 1, 1); // Collision box of the camera
+    camera.minZ = 0.1; // Helps with camera clipping
 
-    return camera
+
+    return camera;
 }
 
 const setupLight = (scene: Scene): HemisphericLight => {
-    const light = new HemisphericLight("light", LIGHT_CONFIG.position, scene);
-    light.intensity = LIGHT_CONFIG.intensity;
+    const light = new HemisphericLight("light", SCENE_CONFIG.LIGHT_CONFIG.position, scene);
+    light.intensity = SCENE_CONFIG.LIGHT_CONFIG.intensity;
+
     return light;
 }
 
@@ -52,7 +68,6 @@ const setupLight = (scene: Scene): HemisphericLight => {
 
 const CreateBasicScene = () => {
     const reactCanvas = useRef(null); // Use useRef to store the canvas element
-
 
     useEffect(() => {
         const canvas = reactCanvas.current; // Get the canvas element
@@ -63,25 +78,29 @@ const CreateBasicScene = () => {
 
         // Init
         const engine = new Engine(canvas, true) // First argument is the canvas element, second argument is antialiasing
-        const scene = new Scene(engine) // Create a new scene
 
-        const player = createPlayer(scene)
-        setupCamera(scene, canvas, player)
+        const scene = new Scene(engine) // Create a new scene
+        scene.clearColor = new Color4(0.5, 0.5, 0.5, 1);
+        scene.gravity = new Vector3(0, GRAVITY/FPS, 0); // Gravity in babylong is measured in units/frame
+
         setupLight(scene)
-        
+        setupCamera(scene, canvas)
+
         // Objects
         const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene) 
         sphere.position.y = 20 // Same as the light
-
         
         const box = MeshBuilder.CreateBox("box", { size: 2 }, scene) 
         box.position.y = 1
+        box.checkCollisions = true;
 
-        MeshBuilder.CreateGround(
+        const ground = MeshBuilder.CreateGround(
             "ground",
-            GROUND_CONFIG,
+            SCENE_CONFIG.GROUND_CONFIG,
             scene
-        )
+        );
+
+        ground.checkCollisions = true;
 
 
         engine.runRenderLoop(() => { 
