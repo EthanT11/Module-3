9c1b8b203410c2a082e3d5221656deae0c834760
Ethan <61360788+EthanT11@users.noreply.github.com>
2025-02-15 17:09:02 -0330

    feat: Added Tailwind to start making a start screen. Made the linux-x64-gnu rollup plugin an optionalDependency since it's only for railway deployment.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import CreateEnvironment from "./components/CreateEnvironment";
 
+const StartScreen = () => (
+  <div className="flex flex-col items-center justify-center h-screen">
+    <h1 className="text-4xl font-bold text-center text-black">Start Screen</h1>
+  </div>
+);
+
 export default () => (
   <div>
-    <CreateEnvironment />
+    <StartScreen />
+    {/* <CreateEnvironment /> */}
   </div>
 );
