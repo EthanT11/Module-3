c88f758ad4326b5f96a75d87153590ddefe2d63e
Ethan <61360788+EthanT11@users.noreply.github.com>
2025-05-13 16:47:35 -0230

    feat(startScreen): Added some styling to the Start Screen.

--- packages/frontend/src/game/menu/start_screen/createStartScreen.ts
+++ packages/frontend/src/game/menu/start_screen/createStartScreen.ts
@@ -1,5 +1,5 @@
 import { Engine, Scene, Vector3, Color4, ArcRotateCamera, Color3 } from "@babylonjs/core";
-import { AdvancedDynamicTexture, Button, InputText, TextBlock } from "@babylonjs/gui";
+import { AdvancedDynamicTexture, Button, InputText, TextBlock, Rectangle, StackPanel } from "@babylonjs/gui";
 import fillMap from "../../map/utility/fillMap";
 import { createLight, createSkyBox, createGround, createFog } from "../../map/map_objects";
 import { startScreenMap } from "../../map/utility/startScreenMaps";
@@ -42,62 +42,137 @@ export const createBabylonStartScreen = (canvas: HTMLCanvasElement, props: Babyl
   // startScreenMap instead of maze for the Punch Maze Title Screen
   fillMap([...maze].reverse(), startScreenScene);
 
-  // Start screen UI
-  const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI", true, startScreenScene);
+
+  const startScreenUI = AdvancedDynamicTexture.CreateFullscreenUI("UI", true, startScreenScene);
   
   const titleText = new TextBlock();
   titleText.text = "PUNCH MAZE";
   titleText.color = "white";
   titleText.fontSize = 50;
-  titleText.top = "-150px";
-  titleText.shadowColor = "black";
+  // titleText.top = "-150px";
+  titleText.top = "-25%";
+  titleText.shadowColor = "yellow";
   titleText.shadowBlur = 10;
   titleText.shadowOffsetX = 2;
   titleText.shadowOffsetY = 2;
-  advancedTexture.addControl(titleText);
+  startScreenUI.addControl(titleText);
 
+  // Menu Container
+  const menuContainer = new Rectangle();
+  menuContainer.width = "300px";
+  menuContainer.height = "450px";
+  menuContainer.background = "rgba(0, 0, 0, 0.3)";
+  menuContainer.cornerRadius = 10;
+  menuContainer.top = "5%";
+  startScreenUI.addControl(menuContainer);
+
+  // StackPanel for name input section
+  const topStack = new StackPanel();
+  topStack.width = 1;
+  topStack.height = 1;
+  topStack.isVertical = true;
+  topStack.paddingTop = "20px";
+  topStack.paddingLeft = "20px";
+  topStack.paddingRight = "20px";
+  topStack.spacing = 15;
+  menuContainer.addControl(topStack);
 
   // Contestant name
   const contestantNameText = new TextBlock();
   contestantNameText.text = "Contestant Name";
   contestantNameText.color = "white";
   contestantNameText.fontSize = 20;
-  contestantNameText.top = "-100px";
-  advancedTexture.addControl(contestantNameText);
-
+  contestantNameText.height = "30px";
+  contestantNameText.textHorizontalAlignment = TextBlock.HORIZONTAL_ALIGNMENT_CENTER;
+  topStack.addControl(contestantNameText);
+  
   // Name input
   const nameInput = new InputText();
-  nameInput.width = "200px";
-  nameInput.height = "50px";
+  nameInput.width = 1;
+  nameInput.height = "40px";
   nameInput.color = "white";
   nameInput.fontSize = 20;
-  nameInput.top = "-50px";
-  advancedTexture.addControl(nameInput);
+  topStack.addControl(nameInput);
+
+  // Name check text
+  const nameCheckText = new TextBlock();
+  nameCheckText.text = "Name must be at least 3 characters long and less than 10 characters";
+  nameCheckText.color = "white";
+  nameCheckText.fontSize = 15;
+  nameCheckText.width = 1;
+  nameCheckText.height = 0.3;
+  nameCheckText.textWrapping = true;
+  nameCheckText.textHorizontalAlignment = TextBlock.HORIZONTAL_ALIGNMENT_CENTER;
+  topStack.addControl(nameCheckText);
+
+
+  // Divider
+  const divider = new Rectangle();
+  divider.width = 1;
+  divider.height = "2px";
+  divider.color = "white";
+  divider.cornerRadius = 10;
+  menuContainer.addControl(divider);
+
+  // Bottom StackPanel for buttons section
+  const bottomStack = new StackPanel();
+  bottomStack._automaticSize = true;
+  bottomStack.width = 1;
+  bottomStack.height = 1;
+  bottomStack.isVertical = true;
+  bottomStack.paddingLeft = "20px";
+  bottomStack.paddingRight = "20px";
+  bottomStack.spacing = 15;
+  bottomStack.top = "60%";
+  menuContainer.addControl(bottomStack);
 
   // Join Match button
   const joinMatchButton = Button.CreateSimpleButton("joinMatchButton", "Join Match");
-  joinMatchButton.width = "200px";
+  joinMatchButton.width = 1;
   joinMatchButton.height = "50px";
   joinMatchButton.color = "white";
   joinMatchButton.background = "green";
-  joinMatchButton.top = "0px";
-  advancedTexture.addControl(joinMatchButton);
+  joinMatchButton.cornerRadius = 10;
+  bottomStack.addControl(joinMatchButton);
 
   // Start button
   const startButton = Button.CreateSimpleButton("createMatchButton", "Create Match");
-  startButton.width = "200px";
+  startButton.width = 1;
   startButton.height = "50px";
   startButton.color = "white";
   startButton.background = "green";
-  startButton.paddingBottom = "10px";
-  startButton.paddingTop = "10px";
   startButton.cornerRadius = 10;
-  startButton.top = "100px";
-  advancedTexture.addControl(startButton);
+  bottomStack.addControl(startButton);
 
-  
+  // SelfPlug
+  // TODO: Add github and normal selfplug things
+  // Just wanted to create the space to know how to account for it
+  const selfPlug = new TextBlock();
+  selfPlug.text = "Made with ❤️ by Ethan Tracey \n GetBuilding 2025";
+  selfPlug.textWrapping = true;
+  selfPlug.color = "white";
+  selfPlug.fontSize = 15;
+  selfPlug.height = "40px";
+  selfPlug.textHorizontalAlignment = TextBlock.HORIZONTAL_ALIGNMENT_CENTER;
+  bottomStack.addControl(selfPlug);
+
+  const handleInputText = (input: InputText): boolean => {
+    console.log(input.text);
+    return input.text.length > 0;
+  }
+
+  // Start button on click
   startButton.onPointerClickObservable.add(() => {
-    props.onStart();
+    const textCheck = handleInputText(nameInput);
+    nameInput.background = textCheck ? "green" : "red";
+    if (textCheck) {
+      // props.onStart();
+    }
+  });
+
+  nameInput.onTextChangedObservable.add(() => {
+    const textCheck = handleInputText(nameInput);
+    nameInput.background = textCheck ? "green" : "red";
   });
   
   
