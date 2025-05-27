ee1eeedd243bc6649b101464dae4ee922b48524b
Ethan <61360788+EthanT11@users.noreply.github.com>
2025-05-27 16:31:23 -0230

    feat: Implemented new character with all the game animations and changed from first to 3rd person controller.

--- packages/frontend/src/game/lobby/CreateLobby.tsx
+++ packages/frontend/src/game/lobby/CreateLobby.tsx
@@ -1,7 +1,6 @@
-import { Engine, Scene, UniversalCamera, Vector3, HemisphericLight, MeshBuilder, Color4, Color3 } from "@babylonjs/core"
+import { Engine, Scene, UniversalCamera, Vector3, HemisphericLight, MeshBuilder, Color4, Color3, AbstractMesh, LoadAssetContainerAsync, AssetContainer, AnimationGroup, TransformNode, Matrix, FollowCamera, PointerEventTypes } from "@babylonjs/core"
 import { useRef, useEffect } from "react"
 // import { setupMultiplayer } from "../../networking/setupMultiplayer";
-// import createPlayerCamera from "../player/createPlayerCamera";
 import { setupScene } from "../setupScene";
 import { SCENE_CONFIG } from "../config";
 // import { createPlayer } from "../player/createPlayer";
@@ -21,6 +20,7 @@ const CreateLobby = (): JSX.Element => {
 
     useEffect( () => {
         let engine: Engine;
+        let playerRotationY = 0;
         // TODO: Add a lobby HUD
         // let gameHUD: GameHUD;
 
@@ -46,47 +46,125 @@ const CreateLobby = (): JSX.Element => {
                 scene.ambientColor = new Color3(0.3, 0.3, 0.3);
 
                 try {
-                    const camera = new UniversalCamera(
-                        "playerCamera",
-                        new Vector3(0, 1, 0),
-                        scene
-                    );
-                    camera.attachControl(canvas, true);
-                    camera.inertia = 0.4;
+                    let modelUrl: string;
+                    let modelContainer: AssetContainer;
+                    let playerMesh: AbstractMesh;
+                    let camera: FollowCamera;
+                    let playerTransformNode: TransformNode;
+
+                    interface Animations {
+                        tpose: AnimationGroup;
+                        idle: AnimationGroup;
+                        run: AnimationGroup;
+                        jump: AnimationGroup;
+                        jumpUp: AnimationGroup;
+                        punch: AnimationGroup;
+                        bigPunch: AnimationGroup;
+                        getHit: AnimationGroup;
+                        death: AnimationGroup;
+                        victory: AnimationGroup;
+                    }
+
+                    let animations: Animations;
+                    try {
+                        const { getAssetUrl } = useSupabase();
+                        modelUrl = getAssetUrl("models", "newCharacterModel.glb");
+                        try {
+                            modelContainer = await LoadAssetContainerAsync(
+                                modelUrl,
+                                scene,
+                                {
+                                    pluginExtension: ".glb"
+                                }
+                            )
+                            console.log("Model container loaded:", modelContainer);
+                            modelContainer.meshes.forEach(mesh => {
+                                scene.addMesh(mesh);
+                                console.log("Added mesh to scene:", mesh.name, "Has skeleton:", !!mesh.skeleton);
+                            });
+                            modelContainer.animationGroups.forEach(group => {
+                                // Remove any existing animation groups with the same name
+                                if (scene.animationGroups.find(g => g.name === group.name)) {
+                                    scene.removeAnimationGroup(group);
+                                }
+                                scene.addAnimationGroup(group);
+                                // console.log("Added animation group to scene:", group.name);
+                            });
+                            animations = {
+                                tpose: scene.getAnimationGroupByName("t-pose") as AnimationGroup,
+                                idle: scene.getAnimationGroupByName("idle") as AnimationGroup,
+                                run: scene.getAnimationGroupByName("run") as AnimationGroup,
+                                jump: scene.getAnimationGroupByName("jump") as AnimationGroup,
+                                jumpUp: scene.getAnimationGroupByName("jumpUp") as AnimationGroup,
+                                punch: scene.getAnimationGroupByName("punch1") as AnimationGroup,
+                                bigPunch: scene.getAnimationGroupByName("punch2") as AnimationGroup,
+                                getHit: scene.getAnimationGroupByName("hit") as AnimationGroup,
+                                death: scene.getAnimationGroupByName("death") as AnimationGroup,
+                                victory: scene.getAnimationGroupByName("dance") as AnimationGroup,
+                            }
+                            if (animations.idle) {
+                                animations.idle.play(true);
+                            }
+
+                            playerMesh = scene.getMeshByName("characterMedium") as AbstractMesh;
+                            
+
+                            camera = new FollowCamera("ThirdPersonCam", new Vector3(0, 8, -7), scene);
+                            camera.inertia = 0.4;
+                            camera.fov = 1.2;
+                            camera.minZ = 0.1;
+
+                            // Set the camera to always follow the player mesh
+                            camera.lockedTarget = playerMesh;
+                            camera.rotationOffset = 180; // Always behind the player
+
+                            // Rotation handling
+                            scene.onPointerObservable.add((pointerInfo) => {
+                                if (pointerInfo.type === PointerEventTypes.POINTERMOVE && document.pointerLockElement) {
+                                    const deltaX = pointerInfo.event.movementX || 0;
+                                    const sensitivity = 0.003;
+                                    playerRotationY += deltaX * sensitivity;
+
+                                    // Normalize
+                                    playerRotationY = (playerRotationY + Math.PI * 2) % (Math.PI * 2);
+                                    playerMesh.rotation.y = playerRotationY;
+                                }
+                            });
+                        } catch (error) {
+                            console.error("CreateLobby: Error fetching modelContainer", error);
+                        }
+                        
+                    } catch (error) {
+                        console.error("CreateLobby: Error loading model", error);
+                    }
+
+
+                    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+                    light.intensity = 0.5;
+                    const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
+                    ground.position.y = 0;
+                    // Initialize the player state manager
+                    // const playerStateManager = new PlayerStateManager();
+
+                    // Setup the player
+                    // const camera = createPlayerCamera(scene, canvas);
+                    // createPlayer(scene, camera, playerStateManager); // TODO: Probably consolidate this into the setupPlayerCamera function
+                    
+                    // TODO: Add a lobby HUD
+                    
+                    // Setup the multipldeathand map
+                    // if (room) {
+                    //     setupMultiplayer(scene, camera, playerStateManager, room);
+                    //     // TODO: Create a lobby Map
+                    //     loadMap(scene, playerStateManager, isHost, room);
+                    // } else {
+                    //     // If failed redirect to main menu
+                    //     console.error("CreateEnvironment: Current room is not found");
+                    //     navigate("/");
+                    // }
                 } catch (error) {
                     console.error("CreateLobby: Error setting up camera", error);
                 }
-
-                try {
-                    const { getAssetUrl } = useSupabase();
-                    const modelUrl = getAssetUrl("models", "newCharacterModel.glb");
-                } catch (error) {
-                    console.error("CreateLobby: Error setting up model", error);
-                }
-
-                const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-                light.intensity = 0.5;
-                const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
-                ground.position.y = 0;
-                // Initialize the player state manager
-                // const playerStateManager = new PlayerStateManager();
-
-                // Setup the player
-                // const camera = createPlayerCamera(scene, canvas);
-                // createPlayer(scene, camera, playerStateManager); // TODO: Probably consolidate this into the setupPlayerCamera function
-                
-                // TODO: Add a lobby HUD
-                
-                // Setup the multiplayer and map
-                // if (room) {
-                //     setupMultiplayer(scene, camera, playerStateManager, room);
-                //     // TODO: Create a lobby Map
-                //     loadMap(scene, playerStateManager, isHost, room);
-                // } else {
-                //     // If failed redirect to main menu
-                //     console.error("CreateEnvironment: Current room is not found");
-                //     navigate("/");
-                // }
 
                 scene.executeWhenReady(() => {
                     engine.loadingScreen.hideLoadingUI();
